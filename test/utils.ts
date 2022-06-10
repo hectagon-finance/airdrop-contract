10ead4891872a17cff93d9b6a55f1bebeeef04ca
Mount Junga <jungamount@gmail.com>
2022-06-10 09:28:41 +0700

    Task: Refactor airdrop

--- test/utils.ts
+++ test/utils.ts
@@ -1,9 +1,11 @@
 import { ethers } from "hardhat";
 
-export function generateLeaf(address: string, value: string): Buffer {
+export function generateLeaf(address: string, rewardId: string, value: string): Buffer {
     return Buffer.from(
         // Hash in appropriate Merkle format
-        ethers.utils.solidityKeccak256(["address", "uint256"], [address, value]).slice(2),
+        ethers.utils
+            .solidityKeccak256(["address", "uint256", "uint256"], [address, rewardId, value])
+            .slice(2),
         "hex"
     );
 }
